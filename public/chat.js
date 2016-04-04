14ff36c6288fbbd0c744bb9053933c34715ff343
Michael Laythe <mrlaythe24@aol.com>
2016-04-04 11:44:44 -0700

    Adds name of user along with their message

--- public/chat.js
+++ public/chat.js
@@ -1,25 +1,38 @@
-window.onload = function(){
-  var messages = [],
-      socket = io.connect('http://localhost:5000'),
-      field = document.getElementById('field'),
-      sendButton = document.getElementById('send'),
-      content = document.getElementById('content');
+$(document).ready(function() {
+	let messages = [],
+	 		socket = io.connect('http://'+location.host),
+	 		field = document.getElementById('field'),
+	 		sendButton = document.getElementById('send'),
+	 		content = document.getElementById('content'),
+	 		name = document.getElementById('name');
 
-   socket.on('message', function(data){
-     if(data.message) {
-       messages.push(data.message);
-       var html = '';
-       for(var i = 0; i < messages.length; i++){
-         html += messages[i] + '<br />';
-       }
-       content.innerHTML = html;
-     } else {
-       console.log('There are issues going on: ', data);
-     }
-   });
+	socket.on('message', function (data) {
+		if(data.message) {
+			messages.push(data);
+			var html = '';
+			for(var i=0; i<messages.length; i++) {
+				html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
+				html += messages[i].message + '<br />';
+			}
+			content.innerHTML = html;
+		} else {
+			console.log('There is a problem:', data);
+		}
+	});
 
-   sendButton.onclick = function(){
-     var text = field.value;
-     socket.emit('send', {message: text});
-   };
-};
+	sendButton.onclick = sendMessage = function() {
+		if(name.value === '') {
+			alert('Please type your name!');
+		} else {
+			var text = field.value;
+			socket.emit('send', { message: text, username: name.value });
+			field.value = '';
+		}
+	};
+
+	$('#field').keyup(function(e) {
+		if(e.keyCode === 13) {
+			sendMessage();
+		}
+	});
+});
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 var port = 5000;
 
-app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.static(__dirname + '/public'));
 
 app.set('views', __dirname + '/template');
-app.set('view engine', "jade");
+app.set('view engine', 'jade');
 app.engine('jade', require('jade').__express);
 
-app.get("/", function(req, res){
-    res.render("page");
+app.get('/', function(req, res){
+    res.render('page');
 });
 
 var io = require('socket.io').listen(app.listen(port, function(){
