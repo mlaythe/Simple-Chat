5ed2fc8523b5d56f61afc79f6da133d1d0005c1d
Michael Laythe <mrlaythe24@aol.com>
2016-04-05 11:20:52 -0700

    Added some bootstrap, changed it so cursor automatically goes to input message field upon loading, & encapsulated the time variables into own function

--- public/chat.js
+++ public/chat.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
-	var messages = [],
+	let messages = [],
 	 		socket = io.connect('http://'+location.host),
 	 		field = document.getElementById('field'),
 	 		sendButton = document.getElementById('send'),
 	 		content = document.getElementById('content'),
-	 		name = document.getElementById('name');
+			name = prompt("Enter username for chat");
 
 	socket.on('message', function (data) {
 		if(data.message) {
 			messages.push(data);
-			var html = '';
-			var d = new Date();
-			var timeSubmitted = ` ${d.getMonth()}-${d.getDay()} ${d.getHours()}:${d.getMinutes()} pst`;
-			for(var i=0; i<messages.length; i++) {
+			let html = '',
+				  timeSubmitted = getTime(),
+					i = 0, length = messages.length;
+
+			for(i; i < length; i++) {
 				html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
 				html += messages[i].message  + '<div style="font-size:80%; float:right;">' + timeSubmitted + '</div>\n' + '<br />';
 			}
@@ -23,11 +24,12 @@ $(document).ready(function() {
 	});
 
 	sendButton.onclick = sendMessage = function() {
-		if(name.value === '') {
-			alert('Please type your name!');
+		if(name === '') {
+			field.value = '';
+			name = prompt('Please type your name!');
 		} else {
-			var text = field.value;
-			socket.emit('send', { message: text, username: name.value });
+			let text = field.value;
+			socket.emit('send', { message: text, username: name });
 			field.value = '';
 		}
 	};
@@ -37,4 +39,22 @@ $(document).ready(function() {
 			sendMessage();
 		}
 	});
+
+	setFocus();
 });
+
+function setFocus () {
+	var input = document.getElementById("field");
+	input.focus();
+}
+
+function getTime() {
+	let d = new Date(),
+			hours = d.getHours(),
+			month = d.getMonth(),
+			minutes = d.getMinutes(),
+			monthNames = ["January", "February", "March", "April", "May", "June",
+										"July", "August", "September", "October", "November", "December"];
+	hours = hours > 12 ? hours - 12: hours;
+	return ` ${monthNames[d.getMonth()]} ${d.getDay()} ${hours}:${minutes} pst`;
+}
